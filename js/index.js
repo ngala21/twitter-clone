f253b2c8177f3cb584245c461ada86a78add8d95
ngala21 <ngalamike21@gmail.com>
2026-06-22 15:07:28 +0300

    feat: storing tweets on firestore

--- js/index.js
+++ js/index.js
@@ -1,20 +1,68 @@
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-// User is signed in, see docs for a list of available properties
-// https://firebase.google.com/docs/reference/js/v8/firebase.User
-// var uid = user.uid;
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/v8/firebase.User
+    var uid = user.uid;
 
-document.getElementById("logout").onclick = () => {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      // Sign-out successful.
-      window.location.href = "signin.html";
-    })
-    .catch((error) => {
-      // An error happened.
-    });
-};
-//   }
-// });
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(uid)
+      .get()
+      .then((userDoc) => {
+        let user = userDoc.data();
+        // console.log(user);
+
+        document.getElementById("post").onclick = () => {
+          let tweet = document.getElementById("tweet").ariaValueMax;
+          // create a tweet collection
+          firebase
+            .firestore()
+            .collection("tweets")
+            .doc()
+            .set({
+              tweet,
+              name: user.name,
+              userId: uid,
+              createdOn: Date.now(),
+            })
+            .then(() => {
+              window.location.reload();
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        };
+        // console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // Fetch the tweet collection
+    firebase
+      .firestore()
+      .collection("tweets")
+      .get()
+      .then((queryTweets) => {
+        queryTweets.forEach((tweetDoc) => {
+          let tweet = tweetDoc.data();
+          console.log(tweet);
+        });
+      });
+    document.getElementById("logout").onclick = () => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          // Sign-out successful.
+          window.location.href = "signin.html";
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+    };
+  } else {
+    window.location.href = "signin.html";
+  }
+});
